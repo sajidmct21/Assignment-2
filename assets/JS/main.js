71e104e08094a4507192a0889b05d75aa4b96ae5
sajidmct21 <sajidmct21@gmail.com>
2024-05-21 13:39:59 +0500

    Assignment has completed

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,36 @@
-const menuIcon = document.querySelector('.menu-icon');
-const nav = document.querySelector('ul')
-menuIcon.addEventListener('click',()=>{
-    nav.classList.toggle('showData');
-})
+// const menuIcon = document.querySelector('.menu-icon');
+// const nav = document.querySelector('ul')
+// menuIcon.addEventListener('click',()=>{
+//     nav.classList.toggle('showData');
+// })
+
+//    Initialize Swiper 
+
+    var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 15,
+        stretch: 0,
+        depth: 400,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+
+    //  Initialize Swiper 2
+
+      var swiper = new Swiper(".mySwiper-2", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+    
